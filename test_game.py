6de3b0c4a622b643e6ffd238d969a37cd5e723ba
sgmath12 <sgmath12@gmail.com>
2026-07-01 14:48:25 +0900

    [refactoring] page up to 38

--- test_game.py
+++ test_game.py
@@ -5,11 +5,14 @@ from game import Game
 def game():
     return Game()   
 
-def test_exception_when_input_is_none(game):
- 
-    with pytest.raises(TypeError):
-        game.guess(None)
+def assert_illegal_argument(game, guessNumber):
+    try:
+       game.guess(guessNumber)
+       pytest.fail("Expected TypeError for input length less than 3")
+    except:
+        pass
 
-def test_exception_when_input_length_is_unmatched(game):
-    with pytest.raises(TypeError):
-        game.guess("12")  # Assuming the expected length is not 4
+def test_exception_when_invalid_input(game):
+    assert_illegal_argument(game, None)
+    assert_illegal_argument(game, "12")  # Input length is 2, which is less than expected
+    assert_illegal_argument(game, "1234")  # Input length is 4, which is more than expected
