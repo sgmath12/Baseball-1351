5dac64480d4932aa71f8647c1a070c035039c5f1
sgmath12 <sgmath12@gmail.com>
2026-07-01 15:20:12 +0900

    [refactoring] final page

--- test_game.py
+++ test_game.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+from typing import Literal
+from unittest import result
+
 import pytest
 from game import Game
 from game_result import GameResult
@@ -11,23 +16,19 @@ def assert_illegal_argument(game, guess_number):
         game.guess(guess_number)
 
 @pytest.mark.parametrize("invalid_input", [None, "12", "1234","12s", "121"])
-def test_exception_when_invalid_input(game, invalid_input):
+def test_exception_when_invalid_input(game: Game, invalid_input: None | Literal['12'] | Literal['1234'] | Literal['12s'] | Literal['121']):
     assert_illegal_argument(game, invalid_input)
 
-def test_return_solved_result_if_mathced_number(game):
-    game.question = "123"
-    result : GameResult = game.guess("123")
-
+def assert_matched_number(result, solved, strikes, balls):
     assert result is not None
-    assert result.solved == True
-    assert result.strikes == 3
-    assert result.balls == 0
+    assert result.solved == solved
+    assert result.strikes == strikes
+    assert result.balls == balls
 
-def test_return_solved_result_if_unmathced_number(game):
+def test_return_solved_result_if_mathced_number(game: Game):
     game.question = "123"
-    result : GameResult = game.guess("456")
+    assert_matched_number(game.guess("123"), solved=True, strikes=3, balls=0)
 
-    assert result is not None
-    assert result.solved == False
-    assert result.strikes == 0
-    assert result.balls == 0
+def test_return_solved_result_if_unmathced_number(game: Game):
+    game.question = "123"
+    assert_matched_number(game.guess("456"), solved=False, strikes=0, balls=0)
