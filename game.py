29a4968222ee3ebe371c79f994587ddd83ba6ef5
sgmath12 <sgmath12@gmail.com>
2026-07-01 14:44:32 +0900

    [feature] 31 page

--- game.py
+++ game.py
@@ -2,3 +2,5 @@ class Game:
     def guess(self, guessNumber):
         if guessNumber is None:
             raise TypeError()
+        if len(guessNumber) != 3:  # Assuming the expected length is 3
+            raise TypeError()
