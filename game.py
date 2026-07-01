d1d41309d6d23e3e8ae0eb3047dccdb50631354f
sgmath12 <sgmath12@gmail.com>
2026-07-01 14:52:29 +0900

    [feature] page up to 40

--- game.py
+++ game.py
@@ -4,3 +4,6 @@ class Game:
             raise TypeError()
         if len(guessNumber) != 3:  # Assuming the expected length is 3
             raise TypeError()
+        for number in guessNumber:
+            if not ord('0') <= ord(number) <= ord('9'):
+                raise TypeError()
--- test_game.py
+++ test_game.py
@@ -8,11 +8,12 @@ def game():
 def assert_illegal_argument(game, guessNumber):
     try:
        game.guess(guessNumber)
-       pytest.fail("Expected TypeError for input length less than 3")
-    except:
+       pytest.fail()
+    except TypeError:
         pass
 
 def test_exception_when_invalid_input(game):
     assert_illegal_argument(game, None)
     assert_illegal_argument(game, "12")  # Input length is 2, which is less than expected
-    assert_illegal_argument(game, "1234")  # Input length is 4, which is more than expected
+    assert_illegal_argument(game, "1234")
+    assert_illegal_argument(game, "12s")  # Input length is 4, which is more than expected
